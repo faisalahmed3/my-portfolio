69090cafefae876b3684abd11954c1b883e5ce49
faisalahmed3 <faisalahmed4417@gmail.com>
2026-01-12 16:46:00 +0600

    certificate section view certificate button fixed

--- src/components/sections/Certificates.js
+++ src/components/sections/Certificates.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEye  } from "react-icons/fa";
 
 const certificates = [
   {
@@ -127,32 +127,12 @@ export default function Certificates() {
                         transition
                       "
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.458 12C3.732 7.943 7.523 5 12 5
-                            c4.478 0 8.268 2.943 9.542 7
-                            -1.274 4.057-5.064 7-9.542 7
-                            -4.477 0-8.268-2.943-9.542-7z"
-                        />
-                      </svg>
+                      <FaEye className="text-sm" />
                       View Certificate
                     </div>
                   </div>
                 </div>
+
 
                 {/* CONTENT */}
                 <div className="p-4">
