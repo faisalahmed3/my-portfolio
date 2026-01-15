d3ff916d6621d16036b3e25972e115bc5ca3ca15
faisalahmed3 <faisalahmed4417@gmail.com>
2026-01-15 12:49:52 +0600

    meta data and icon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Faisal Ahmed",
+  description: "Full Stack Developer Portfolio",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
